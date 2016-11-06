b74e7dbbf3fcf306180e6b1290504468415eeccf
geekSiddharth <siddharth16268@iiitd.ac.in>
2016-11-07 03:48:52 +0530

    Updated subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -1,17 +1,11 @@
 <?php
 
 
-	// Read the form values
-	$Email = $_POST['subscribe'] ;
+	// Read the form values and append a new line
+	$email_address = $_POST['subscribe']."\n" ;
 
-
-	$file = 'list.txt';
-	// Open the file to get existing content
-	$current = file_get_contents($file);
-	// Append a new person to the file
-	$current .= $Email."\n";
-	// Write the contents back to the file
-	file_put_contents($file, $current);
+	// apend $email address to list.txt file
+ 	$myfile = file_put_contents('list.txt', $email_address.PHP_EOL , FILE_APPEND | LOCK_EX);
 
 
 ?>
